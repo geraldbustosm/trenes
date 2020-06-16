fd56d8580447c58f02c27ec046341a2d502353ba
Cml096 <cristobalmontanolopez@gmail.com>
2020-06-16 02:26:51 -0400

    Connection DB

---
+++ Proyecto_Trenes/Proyecto_Trenes/BusinessLogicLayer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Trenes
+{
+    public class BusinessLogicLayer
+    {
+
+        private DataAccessLayer _dataAccessLayer;
+
+        public BusinessLogicLayer()
+        {
+            _dataAccessLayer = new DataAccessLayer();
+        }
+        public Tren saveTren(Tren tren)
+        {
+            if (tren.Id == 0)
+                _dataAccessLayer.insertTren(tren);
+            return tren;
+        }
+
+        public List<Tren> getTrenes()
+        {
+            return _dataAccessLayer.getTrenes();
+        }
+
+    }
+}
---
+++ Proyecto_Trenes/Proyecto_Trenes/DataAccessLayer.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Trenes
+{
+    public class DataAccessLayer
+    {
+        private SqlConnection conn = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;User ID=SA;Initial Catalog=Proyecto-Trenes-DB;Data Source=DESKTOP-EBJ27AI"); 
+        public void insertTren(Tren tren) {
+            try
+            {
+                conn.Open();
+                string query = @"
+                                INSERT INTO Trenes (capacidad, codigo, tipoMaterial, origen, destino)
+                                VALUES (@tipo, @origen, @destino, @capacidad, @codigo)
+                                ";
+                SqlParameter tipoMaterial = new SqlParameter("@tipo", tren.tipo);
+                SqlParameter origen = new SqlParameter("@origen", tren.origen);
+                SqlParameter destino = new SqlParameter("@destino", tren.destino);
+                SqlParameter capacidad = new SqlParameter("@capacidad", tren.capacidad);
+                SqlParameter codigo = new SqlParameter("@codigo", tren.codigo);
+
+                SqlCommand command = new SqlCommand(query, conn);
+
+                command.Parameters.Add(tipoMaterial);
+                command.Parameters.Add(origen);
+                command.Parameters.Add(destino);
+                command.Parameters.Add(capacidad);
+                command.Parameters.Add(codigo);
+
+                command.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public List<Tren> getTrenes()
+        {
+            List<Tren> trenes = new List<Tren>();
+            try
+            {
+                conn.Open();
+                string query = @"SELECT *
+                                FROM Trenes";
+                SqlCommand command = new SqlCommand(query, conn);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    trenes.Add(new Tren
+                    {
+                        Id = int.Parse(reader["Id"].ToString()),
+                        capacidad = reader["capacidad"].ToString(),
+                        codigo = reader["codigo"].ToString(),
+                        tipo = reader["tipoMaterial"].ToString(),
+                        origen = reader["origen"].ToString(),
+                        destino = reader["destino"].ToString()
+                    });
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return trenes;
+        }
+    }
+}
