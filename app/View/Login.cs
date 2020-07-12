88e3ef01f5835de19bf42ebbd008e062e537a0df
Gerald Bustos Morales <geraldbustosm@gmail.com>
2020-07-11 23:08:01 -0400

    add login view

--- app/View/Login.cs
+++ app/View/Login.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace View
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- app/View/Program.cs
+++ app/View/Program.cs
@@ -15,7 +15,7 @@ namespace View
             DatabaseUtility.ResetDatabase();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
