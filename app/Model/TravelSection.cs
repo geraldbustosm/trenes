e9687a97dab6b3aa2cf896200ab864e2389b3d6c
Cml096 <cristobalmontanolopez@gmail.com>
2020-07-26 05:12:55 -0400

    Change the name and one variable

--- app/Model/TravelSection.cs
+++ app/Model/TravelSection.cs
@@ -5,23 +5,23 @@ using System.Data.SQLite;
 
 namespace Model
 {
-    class TravelAction
+    class TravelSection
     {
         private int id;
         private string arrival_time;
         private int travel_id;
-        private int order;
+        private int priority;
         private int action;
         private int origin_station_id;
         private int destination_station_id;
         private Boolean deleted;
 
-        public TravelAction (int id, string arrival_time, int travel_id, int order, int action, int origin_station_id, int destination_station_id)
+        public TravelSection (int id, string arrival_time, int travel_id, int priority, int action, int origin_station_id, int destination_station_id)
         {
             this.id = id;
             this.arrival_time = arrival_time;
             this.travel_id = travel_id;
-            this.order = order;
+            this.priority = priority;
             this.action = action;
             this.origin_station_id = origin_station_id;
             this.destination_station_id = destination_station_id;
@@ -33,14 +33,14 @@ namespace Model
         public int getId() { return this.id; }
         public string getArrival_time() { return this.arrival_time; }
         public int getTravel_id() { return this.travel_id; }
-        public int getOrder() { return this.order; }
+        public int getPriority() { return this.priority; }
         public int getAction() { return this.action; }
         public int getOrigin_station_id() { return this.origin_station_id; }
         public int getDestination_station() { return this.destination_station_id; }
 
         public void setArrival_time(string arrival_time) { this.arrival_time = arrival_time; }
         public void setTravel_id(int travel_id) { this.travel_id = travel_id; }
-        public void setOrder(int order) { this.order = order; }
+        public void setPriority(int priority) { this.priority = priority; }
         public void setAction(int action) { this.action = action; }
         public void setOrigin_station_id(int origin_station_id) { this.origin_station_id = origin_station_id; }
         public void setDestination_station_id(int destination_station_id) { this.destination_station_id = destination_station_id; }
@@ -55,13 +55,13 @@ namespace Model
 
                 if (!exist)
                 {
-                    string query = "INSERT INTO travel_section(id, arrival_time, travel_id, order, action, origin_station_id, destination_station_id) values (" + this.id + "," + this.arrival_time + "," + this.travel_id + "," + this.order + "," + this.action + "," + this.origin_station_id + "," + this.destination_station_id + ")";
+                    string query = "INSERT INTO travel_section(id, arrival_time, travel_id, priority, action, origin_station_id, destination_station_id) values (" + this.id + "," + this.arrival_time + "," + this.travel_id + "," + this.priority + "," + this.action + "," + this.origin_station_id + "," + this.destination_station_id + ")";
                     db.CommandText = query;
                     db.ExecuteNonQuery();
                 }
                 else
                 {
-                    string query = "UPDATE travel_section SET id = " + this.id + ", arrival_time = " + this.arrival_time + ", travel_id =" + this.travel_id + ",order =" + this.order + ", action =" + this.action + ",origin_station_id =" + this.origin_station_id + ",destination_station_id =" + this.destination_station_id + ") WHERE ID=" + this.id;
+                    string query = "UPDATE travel_section SET id = " + this.id + ", arrival_time = " + this.arrival_time + ", travel_id =" + this.travel_id + ",priority =" + this.priority + ", action =" + this.action + ",origin_station_id =" + this.origin_station_id + ",destination_station_id =" + this.destination_station_id + ") WHERE ID=" + this.id;
                     db.CommandText = query;
                     db.ExecuteNonQuery();
                 }
@@ -82,7 +82,7 @@ namespace Model
 
         // Static Methods
 
-        public static TravelAction find(int id)
+        public static TravelSection find(int id)
         {
 
             SQLiteConnection connection = DatabaseUtility.connection();
@@ -95,12 +95,12 @@ namespace Model
             {
                 string arrival_time = reader.GetString(1);
                 int travel_id = reader.GetInt32(2);
-                int order = reader.GetInt32(3);
+                int priority = reader.GetInt32(3);
                 int action = reader.GetInt32(4);
                 int origin_station_id = reader.GetInt32(5);
                 int destination_station_id = reader.GetInt32(6);
 
-                return new TravelAction(id, arrival_time, travel_id, order, action, origin_station_id, destination_station_id);
+                return new TravelSection(id, arrival_time, travel_id, priority, action, origin_station_id, destination_station_id);
             }
             connection.Close();
             return null;
