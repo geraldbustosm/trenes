a1fcbc3b8c191ed06de22fea492376661da79464
Cml096 <cristobalmontanolopez@gmail.com>
2020-08-08 23:29:57 -0400

    fix SectionAction model

--- app/Model/SectionAction.cs
+++ app/Model/SectionAction.cs
@@ -6,16 +6,15 @@ namespace Model
 {
     class SectionAction
     {
-        private int id { get; }
-        private int action_id { get; }
-        private int travel_section_id { get; }
-        private int locomotive_id { get; }
-        private int wagon_id { get; }
+        public int section_action_id { get; private set; }
+        public int action_id { get; set; }
+        public int travel_section_id { get; set; }
+        public int locomotive_id { get; set; }
+        public int wagon_id { get; set; }
         private Boolean deleted;
 
-        public SectionAction (int id, int action_id, int travel_section_id, int locomotive_id, int wagon_id)
+        public SectionAction(int action_id, int travel_section_id, int locomotive_id, int wagon_id)
         {
-            this.id = id;
             this.action_id = action_id;
             this.travel_section_id = travel_section_id;
             this.locomotive_id = locomotive_id;
@@ -28,18 +27,23 @@ namespace Model
             {
                 SQLiteConnection connection = DatabaseUtility.GetConnection();
                 SQLiteCommand db = new SQLiteCommand(connection);
-                Boolean exist = this.CheckIfUserExist(this.id);
+                Boolean exist = this.CheckIfExist(this.section_action_id);
+
+                db.Parameters.AddWithValue("@action_id", this.action_id);
+                db.Parameters.AddWithValue("@travel_section_id", this.travel_section_id);
+                db.Parameters.AddWithValue("@locomotive_id", this.locomotive_id);
+                db.Parameters.AddWithValue("@wagon_id", this.wagon_id);
 
                 if (!exist)
                 {
-                    string query = "INSERT INTO section_action (section_action_id, action_id, travel_section_id, locomotive_id, wagon_id) values (" + this.id + "," + this.action_id + "," + this.travel_section_id + "," + this.locomotive_id + "," + this.wagon_id + ")";
-                    db.CommandText = query;
-                    db.ExecuteNonQuery();
+                    db.CommandText = "INSERT INTO section_action (action_id, travel_section_id, locomotive_id, wagon_id) values ( @action_id, @travel_section_id, @locomotive_id, @wagon_id )";
+
+                    this.section_action_id = Convert.ToInt32(db.ExecuteScalar());
                 }
                 else
                 {
-                    string query = "UPDATE section_action SET section_action_id = " + this.id + ", action_id = " + this.action_id + ", travel_section_id = " + this.travel_section_id + ",locomotive_id = " + this.locomotive_id + ",wagon_id = " + this.wagon_id + ") WHERE ID=" + this.id;
-                    db.CommandText = query;
+                    db.CommandText = "UPDATE section_action SET ( action_id = @action_id, travel_section_id = @travel_section_id, locomotive_id = @locomotive_id, wagon_id = @wagon_id ) WHERE section_action_id = @section_action_id";
+                    db.Parameters.AddWithValue("@section_action_id", this.section_action_id);
                     db.ExecuteNonQuery();
                 }
                 connection.Close();
@@ -50,21 +54,27 @@ namespace Model
         {
             SQLiteConnection connection = DatabaseUtility.GetConnection();
             SQLiteCommand db = new SQLiteCommand(connection);
-            string query = "DELETE FROM section_action WHERE ID = " + this.id;
-            db.CommandText = query;
+
+            db.CommandText = "DELETE FROM section_action WHERE sectionAction_id = @sectionAction_id";
+            db.Parameters.AddWithValue("@section_action_id", this.section_action_id);
+
             db.ExecuteNonQuery();
+
             connection.Close();
+
             this.deleted = true;
             return true;
         }
 
         // Static Methods
-        public static SectionAction Find(int id)
+        public static SectionAction Find(int sectionAction_id)
         {
             SQLiteConnection connection = DatabaseUtility.GetConnection();
             SQLiteCommand db = new SQLiteCommand(connection);
-            string query = "SELECT * FROM section_action WHERE ID = " + id;
-            db.CommandText = query;
+
+            db.CommandText = "SELECT * FROM section_action WHERE sectionAction_id = @sectionAction_id";
+            db.Parameters.AddWithValue("@section_action_id", sectionAction_id);
+
             SQLiteDataReader reader = db.ExecuteReader();
 
             while (reader.Read())
@@ -75,20 +85,26 @@ namespace Model
                 int locomotive_id = reader.GetInt32(3);
                 int wagon_id = reader.GetInt32(4);
 
-                return new SectionAction (id, action_id, travel_section_id, locomotive_id, wagon_id);
+                SectionAction sectionAction = new SectionAction(action_id, travel_section_id, locomotive_id, wagon_id);
+                sectionAction.section_action_id = sectionAction_id;
+                return sectionAction;
             }
+
+            reader.Close();
             connection.Close();
             return null;
         }
 
         // Private Methods
 
-        private Boolean CheckIfUserExist(int id)
+        private Boolean CheckIfExist(int sectionAction_id)
         {
             SQLiteConnection connection = DatabaseUtility.GetConnection();
             SQLiteCommand db = new SQLiteCommand(connection);
-            string query = "SELECT COUNT(*) FROM section_action WHERE ID=" + id;
-            db.CommandText = query;
+
+            db.CommandText = "SELECT COUNT(*) FROM section_action WHERE sectionAction_id = @sectionAction_id";
+            db.Parameters.AddWithValue("@section_action_id", sectionAction_id);
+
             SQLiteDataReader reader = db.ExecuteReader();
 
             int count = 0;
@@ -96,6 +112,8 @@ namespace Model
             {
                 count = reader.GetInt32(0);
             }
+
+            reader.Close();
             connection.Close();
             if (count > 0) { return true; } else { return false; }
         }
