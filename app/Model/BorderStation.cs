75170b258028636653b530e0e5df5e9fddfe7886
Cml096 <cristobalmontanolopez@gmail.com>
2020-08-08 22:45:08 -0400

    fix BorderStation model

--- app/Model/BorderStation.cs
+++ app/Model/BorderStation.cs
@@ -6,14 +6,13 @@ namespace Model
 {
     public class BorderStation
     {
-        private int border_station_id { get; }
-        private int station_one_id { get; }
-        private int station_two_id { get; }
+        public int border_station_id { get; private set; }
+        public int station_one_id { get; set; }
+        public int station_two_id { get; set; }
         private Boolean deleted;
 
-        public BorderStation(int border_station_id, int station_one_id, int station_two_id)
+        public BorderStation(int station_one_id, int station_two_id)
         {
-            this.border_station_id = border_station_id;
             this.station_one_id = station_one_id;
             this.station_two_id = station_two_id;
             this.deleted = false;
@@ -29,9 +28,11 @@ namespace Model
 
                 if (!exist)
                 {
-                    string query = "INSERT INTO border_station(border_station_id, station_one_id, station_two_id) values (" + this.border_station_id + "," + this.station_one_id + "," + this.station_two_id + ")";
-                    db.CommandText = query;
-                    db.ExecuteNonQuery();
+                    db.CommandText = "INSERT INTO border_station( station_one_id, station_two_id) VALUES ( @station_one_id , @station_two_id )";
+                    db.Parameters.AddWithValue("@action_id", this.station_one_id);
+                    db.Parameters.AddWithValue("@action_id", this.station_two_id);
+
+                    this.border_station_id = Convert.ToInt32(db.ExecuteScalar());
                 }
                 connection.Close();
             }
@@ -40,21 +41,25 @@ namespace Model
         {
             SQLiteConnection connection = DatabaseUtility.GetConnection();
             SQLiteCommand db = new SQLiteCommand(connection);
-            string query = "DELETE FROM border_station WHERE border_station_id = " + this.border_station_id;
-            db.CommandText = query;
+            db.CommandText = "DELETE FROM border_station WHERE border_station_id = @border_station_id";
+            db.Parameters.AddWithValue("@border_station_id", border_station_id);
+
             db.ExecuteNonQuery();
+
             this.deleted = true;
+
             connection.Close();
             return true;
         }
 
         // Static methods
-        public static BorderStation Find(int id)
+        public static BorderStation Find(int border_station_id)
         {
             SQLiteConnection connection = DatabaseUtility.GetConnection();
             SQLiteCommand db = new SQLiteCommand(connection);
-            string query = "SELECT * FROM border_station WHERE border_station = " + id;
-            db.CommandText = query;
+            db.CommandText = "SELECT * FROM border_station WHERE border_station_id = @border_station_id";
+            db.Parameters.AddWithValue("@border_station_id", border_station_id);
+
             SQLiteDataReader reader = db.ExecuteReader();
 
             while (reader.Read())
@@ -62,27 +67,34 @@ namespace Model
                 int station_one_id = reader.GetInt32(1);
                 int station_two_id = reader.GetInt32(2);
 
-                return new BorderStation(id, station_one_id, station_two_id);
+                return new BorderStation(station_one_id, station_two_id);
             }
+            reader.Close();
             connection.Close();
             return null;
         }
 
         // Private methods
-        private Boolean CheckIfBorderStationExists(int id)
+        private Boolean CheckIfBorderStationExists(int border_station_id)
         {
             SQLiteConnection connection = DatabaseUtility.GetConnection();
             SQLiteCommand db = new SQLiteCommand(connection);
-            string query = "SELECT COUNT(*) FROM border_station WHERE border_station_id=" + id;
-            db.CommandText = query;
+
+            db.CommandText = "SELECT COUNT(*) FROM border_station WHERE border_station_id= @border_station_id";
+            db.Parameters.AddWithValue("@border_station_id", border_station_id);
+
             SQLiteDataReader reader = db.ExecuteReader();
 
             int count = 0;
+
             while (reader.Read())
             {
                 count = reader.GetInt32(0);
             }
+
+            reader.Close();
             connection.Close();
+
             if (count > 0) { return true; } else { return false; }
         }
     }
