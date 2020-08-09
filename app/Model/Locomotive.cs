a97ab7da5002502db89d8e87e10c75ccc23508d8
andrea <andreitaramirez94@gmail.com>
2020-08-09 00:04:03 -0400

    fix locomotive model

--- app/Model/Locomotive.cs
+++ app/Model/Locomotive.cs
@@ -6,17 +6,16 @@ namespace Model
 {
     public class Locomotive
     {
-        private int locomotive_id { get; }
-        private string model { get; set; }
-        private int drag_capacity { get; set; }
-        private int in_transit { get; set; }
-        private int train_id { get; }
-        private int station_id { get; }
-        private Boolean deleted;
+        public int locomotive_id { get; private set; }
+        public string model { get; set; }
+        public int drag_capacity { get; set; }
+        public int in_transit { get; set; }
+        public int train_id { get; set;  }
+        public int station_id { get; set;  }
+        public Boolean deleted;
 
-        public Locomotive(int locomotive_id, string model, int drag_capacity, int in_transit, int train_id, int station_id)
+        public Locomotive(string model, int drag_capacity, int in_transit, int train_id, int station_id)
         {
-            this.locomotive_id = locomotive_id;
             this.model = model;
             this.drag_capacity = drag_capacity;
             this.in_transit = in_transit;
@@ -30,18 +29,23 @@ namespace Model
             {
                 SQLiteConnection connection = DatabaseUtility.GetConnection();
                 SQLiteCommand db = new SQLiteCommand(connection);
-                Boolean exist = this.CheckIfLocomotiveExists(this.locomotive_id);
+                Boolean exist = this.CheckIfExists(this.locomotive_id);
+
+                db.Parameters.AddWithValue("@model", this.model);
+                db.Parameters.AddWithValue("@drag_capacity", this.drag_capacity);
+                db.Parameters.AddWithValue("@in_transit", this.in_transit);
+                db.Parameters.AddWithValue("@train_id", this.train_id);
+                db.Parameters.AddWithValue("@station_id", this.station_id);
 
                 if (!exist)
                 {
-                    string query = "INSERT INTO locomotive(locomotive_id, model, drag_capicity, in_transit, train_id, station_id) values (" + this.locomotive_id + "," + this.model + "," + this.drag_capacity + this.train_id + this.station_id + ")";
-                    db.CommandText = query;
-                    db.ExecuteNonQuery();
+                    db.CommandText = "INSERT INTO locomotive(model, drag_capicity, in_transit, train_id, station_id) values (@model, @drag_capacity, @train_id, @station_id )";
+                    this.locomotive_id = Convert.ToInt32(db.ExecuteScalar());
                 }
                 else
                 {
-                    string query = "UPDATE locomotive SET locomotive_id = " + this.locomotive_id + ", model=" + this.model + ", drag_capacity=" + this.drag_capacity + ",in_transit=" + this.in_transit + ",train_id=" + this.train_id + ",station_id=" + this.station_id + ") WHERE locomotive_id =" + this.locomotive_id;
-                    db.CommandText = query;
+                    db.CommandText = "UPDATE locomotive SET ( model =  @model, drag_capacity = @drag_capacity, in_transit = @in_transit, train_id = @train_id, station_id = @station_id) WHERE locomotive_id = @locomotive_id";
+                    db.Parameters.AddWithValue("@locomotive_id", this.locomotive_id);
                     db.ExecuteNonQuery();
                 }
                 connection.Close();
@@ -52,8 +56,8 @@ namespace Model
         {
             SQLiteConnection connection = DatabaseUtility.GetConnection();
             SQLiteCommand db = new SQLiteCommand(connection);
-            string query = "DELETE FROM locomotive WHERE locomotive_id = " + this.locomotive_id;
-            db.CommandText = query;
+            db.CommandText = "DELETE FROM locomotive WHERE locomotive_id = @locomotive_id";
+            db.Parameters.AddWithValue("@locomotive_id", this.locomotive_id);
             db.ExecuteNonQuery();
             connection.Close();
             this.deleted = true;
@@ -61,12 +65,12 @@ namespace Model
         }
 
         // Static methods
-        public static Locomotive Find(int locomotive_id)
+        public static Locomotive Find(int id)
         {
             SQLiteConnection connection = DatabaseUtility.GetConnection();
             SQLiteCommand db = new SQLiteCommand(connection);
-            string query = "SELECT * FROM locomotive WHERE locomotive_id = " + locomotive_id;
-            db.CommandText = query;
+            db.CommandText = "SELECT * FROM locomotive WHERE locomotive_id = @locomotive_id";
+            db.Parameters.AddWithValue("@locomotive_id",id);
             SQLiteDataReader reader = db.ExecuteReader();
 
             while (reader.Read())
@@ -77,18 +81,21 @@ namespace Model
                 int train_id = reader.GetInt32(4);
                 int station_id = reader.GetInt32(5);
 
-                return new Locomotive(locomotive_id, model, drag_capacity, in_transit, train_id, station_id);
+                Locomotive l = new Locomotive(model, drag_capacity, in_transit, train_id, station_id);
+                l.locomotive_id = id;
+                return l;
             }
+            reader.Close();
             connection.Close();
             return null;
         }
         // Private methods
-        private Boolean CheckIfLocomotiveExists(int locomotive_id)
+        private Boolean CheckIfExists(int id)
         {
             SQLiteConnection connection = DatabaseUtility.GetConnection();
             SQLiteCommand db = new SQLiteCommand(connection);
-            string query = "SELECT COUNT(*) FROM locomotive WHERE locomotive_id=" + locomotive_id;
-            db.CommandText = query;
+            db.CommandText = "SELECT COUNT(*) FROM locomotive WHERE locomotive_id = @locomotive_id";
+            db.Parameters.AddWithValue("@locomotive_id", id);
             SQLiteDataReader reader = db.ExecuteReader();
 
             int count = 0;
@@ -96,8 +103,9 @@ namespace Model
             {
                 count = reader.GetInt32(0);
             }
+            reader.Close();
             connection.Close();
-            if (count > 0) { return true; } else { return false; }
+            return count > 0;
         }
     }
 }
