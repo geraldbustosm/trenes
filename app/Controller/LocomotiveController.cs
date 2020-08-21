3fbb5e6c92df25944088bd5be9e3a0349bd69e1c
Cml096 <cristobalmontanolopez@gmail.com>
2020-08-21 04:38:25 -0400

    Add controller and logic

---
+++ app/Controller/LocomotiveController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Model;
+
+namespace Controller
+{
+    public class LocomotiveController
+    {
+        private List<Locomotive> listLocomotive;
+        public LocomotiveController()
+        {
+            this.listLocomotive = new List<Locomotive>();
+        }
+        public void FeedDataGrid(DataGridView dataGridView)
+        {
+            var source = new BindingSource(this.listLocomotive, null);
+            dataGridView.DataSource = source;
+        }
+        public void AddLocomotive(ComboBox comboBox, string model, string drag_capacity)
+        {
+            int station_id = Convert.ToInt32(comboBox.SelectedValue);
+            int tons_drag = Convert.ToInt32(drag_capacity);
+            Locomotive locomotive = new Locomotive(model, tons_drag, station_id);
+            this.listLocomotive.Add(locomotive);
+        }
+        public void Clear()
+        {
+            this.listLocomotive.Clear();
+        }
+        public bool Insert()
+        {
+            try
+            {
+                foreach (Locomotive one in this.listLocomotive)
+                {
+                    one.Save();
+                }
+                MessageBox.Show("Correcto");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error ...");
+                Console.WriteLine(ex);
+                return false;
+            }
+        }
+        public void DeleteLocomotive(string res)
+        {
+            int id = Convert.ToInt32(res);
+            Locomotive result = this.listLocomotive.Find(delegate (Locomotive l) {
+                return l.locomotive_id == id;
+            });
+            this.listLocomotive.Remove(result);
+        }
+        public static bool IsNumber(string drag_capacity)
+        {
+            try
+            {
+                int cap = Convert.ToInt32(drag_capacity);
+                if (cap >= 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    MessageBox.Show("Error, ingrese una capacidad positiva");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error, ingrese un valor numerico campo capacidad");
+                Console.WriteLine(ex);
+            }
+            return false;
+        }
+        public static void FeedComboBox(ComboBox comboBox)
+        {
+            List<Station> list = Station.All();
+
+            comboBox.DataSource = list;
+            comboBox.DisplayMember = "name";
+            comboBox.ValueMember = "station_id";
+        }
+
+    }
+}
--- app/View/AddLocomotiveForm.cs
+++ app/View/AddLocomotiveForm.cs
@@ -1,14 +1,81 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
+using Controller;
 
 namespace View
 {
     public partial class AddLocomotiveForm : Form
     {
+        private string model;
+        private string drag_capacity;
+        private LocomotiveController locomotiveController;
         public AddLocomotiveForm()
         {
+            this.locomotiveController = new LocomotiveController();
             InitializeComponent();
+        }
+        private void btn_Click(object sender, EventArgs e)
+        {
+            this.model = this.inputModel.Text;
+            this.drag_capacity = this.inputDrag_capacity.Text;
+            if (this.model != null && this.drag_capacity != null)
+            {
+                if (LocomotiveController.IsNumber(this.drag_capacity))
+                {
+                    locomotiveController.AddLocomotive(comboBox, this.model, this.drag_capacity);
+                    locomotiveController.FeedDataGrid(dataGridView);
+                    Clear();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error, Campo Vacío");
+            }
+        }
+        private void Clear()
+        {
+            this.inputModel.Text = "";
+            this.inputDrag_capacity.Text = "";
+        }
+
+        private void AddLocomotiveForm_Load(object sender, EventArgs e)
+        {
+            LocomotiveController.FeedComboBox(comboBox);
+            AddLinkColumn();
+        }
+        private void AddLinkColumn()
+        {
+            DataGridViewLinkColumn link = new DataGridViewLinkColumn();
+
+            link.UseColumnTextForLinkValue = true;
+            link.Name = "Delete";
+            link.Text = "Eliminar";
+
+            dataGridView.Columns.Add(link);
+        }
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.locomotiveController.Clear();
+            this.locomotiveController.FeedDataGrid(dataGridView);
+            Clear();
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (this.locomotiveController.Insert())
+            {
+                this.locomotiveController.Clear();
+                this.locomotiveController.FeedDataGrid(dataGridView);
+            }
+        }
+        private void dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == this.dataGridView.Columns["Delete"].Index)
+            {
+                string res = ((DataGridView)(sender)).Rows[e.RowIndex].Cells[1].Value.ToString();
+                this.locomotiveController.DeleteLocomotive(res);
+                this.locomotiveController.FeedDataGrid(dataGridView);
+            }
         }
     }
 }
