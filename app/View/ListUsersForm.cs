29fc309c8aedca0880d7e3aa84ae952ab7593e8c
Gerald Bustos Morales <geraldbustosm@gmail.com>
2020-08-24 00:04:26 -0400

    fix incorrect delete row

--- app/View/ListUsersForm.cs
+++ app/View/ListUsersForm.cs
@@ -17,12 +17,14 @@ namespace View
         }
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            if(e.ColumnIndex == 0 && this.layout_form.auth.permission_id == 1) { 
-                if (MessageBox.Show("¿Estás seguro que deseas eliminar el registro?", "Ventana de confirmación", MessageBoxButtons.YesNo) == DialogResult.Yes)
-                    {
-                        int id = Int32.Parse(dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString());
-                        UserController.RemoveUserFromDGV(dataGridView1, id, e.RowIndex);
-                    }
+            int id = Int32.Parse(dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString());
+            if (this.layout_form.auth.user_id == id)
+            {
+                ShowWarningMessage();
+            }
+            else
+            {
+                ShowConfirmationMessage(e);
             }
         }
         private void AddActionsToDGV(int rol)
@@ -32,6 +34,22 @@ namespace View
                 UserController.AddEditLinkColumn(dataGridView1);
                 UserController.AddDeleteLinkColumn(dataGridView1);
             }
+        }
+
+        private void ShowConfirmationMessage(DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 1 && this.layout_form.auth.permission_id == 1)
+            {
+                if (MessageBox.Show("¿Estás seguro que deseas eliminar el registro?", "Ventana de confirmación", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    int id = Int32.Parse(dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString());
+                    UserController.RemoveUserFromDGV(dataGridView1, id, e.RowIndex);
+                }
+            }
+        }
+        private void ShowWarningMessage()
+        {
+            MessageBox.Show("No puedes eliminar tu propio usuario");
         }
     }
 }
