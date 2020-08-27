bedb5c949d9c23f0fbec2970f6e391349200f3dd
Cml096 <cristobalmontanolopez@gmail.com>
2020-08-27 00:20:01 -0400

    step-one

--- app/Event/Main.cs
+++ app/Event/Main.cs
@@ -1,8 +1,20 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace Event
 {
     public class Main
     {
+
+        public static void DoWork()
+        {
+            Task.Run(async () => {
+            for(; ; )
+                {
+                    await Task.Delay(10000);
+                    Console.WriteLine("Hi after 10 seconds");
+                }
+            });
+        }
     }
 }
